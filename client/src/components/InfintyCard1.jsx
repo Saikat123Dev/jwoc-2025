847689b4d4e26c05629a9aed25639fe41a387fd0
Saikat123Dev <berasaikat729@gmail.com>
2025-01-21 01:35:06 +0530

    community parter done

--- client/src/components/InfintyCard1.jsx
+++ client/src/components/InfintyCard1.jsx
@@ -8,15 +8,15 @@ const InfinityCard = () => {
     'IIITA.GDSC.png',
     'IITK.GDSC.jpg',
     'NITG.GDSC.jpg',
-    'ProElevate.png'
-    // 'IndiaCollege.jpeg'
+    'ProElevate.png',
+     'IndiaCollege.jpeg'
   ];
 
   // Repeat images to ensure seamless scrolling
   const repeatedImages = [...imageArray, ...imageArray];
 
   return (
-    <div className="relative w-full max-w-full overflow-hidden">
+    <div className="relative  w-full max-w-full overflow-hidden">
       <div className="text-center py-4 sm:py-6">
 
         <h1 className="font-extrabold font-rubik text-xl sm:text-3xl lg:text-5xl text-white">
@@ -29,14 +29,14 @@ const InfinityCard = () => {
           speed={40}
           gradient={false}
           direction="left"
-          className="flex items-center"
+          className="flex  items-center"
         >
           {repeatedImages.map((image, index) => (
-            <div key={index} className="mx-2 sm:mx-4">
+            <div key={index} className="mx-2  sm:mx-4">
               <img
                 src={image}
                 alt={`Community Partner ${index + 1}`}
-                className="h-28 w-28 lg:mx-2  sm:h-28 sm:w-28 lg:h-36 lg:w-36
+                className="h-28 w-28 lg:mx-1  sm:h-28 sm:w-28 lg:h-36 lg:w-36
                   rounded-lg object-contain
                   opacity-75 hover:opacity-100 transition-opacity duration-300"
               />
